01c52c1924acb6f4250aa10c31293711a823dc5c
AlexMagargiu <122966645+AlexMagargiu@users.noreply.github.com>
2023-05-18 20:16:16 +0300

    Complete js

--- javascript.js
+++ javascript.js
@@ -17,4 +17,59 @@ function removeCheckedState() {
       radioInputs[i].checked = false;
     }
 }
-  
+
+function removeCustomAmount(){
+    let customAmount = document.querySelector(".custom-amount");
+    customAmount.value = "";
+}
+
+function calcTipAmountPerPerson(value){
+    let billAmount = document.querySelector(".js-bill-amount");
+    let peopleAmount = document.querySelector(".js-error-input");
+    let customAmount = document.querySelector(".custom-amount");
+    let outputTipPerPerson = document.querySelector(".js-tip-amount");
+    let outputTotalPerPerson = document.querySelector(".js-total-amount");
+    if(billAmount.value !== "" && peopleAmount.value !== "0" && peopleAmount.value !== "" && value !== peopleAmount.value){
+        let calcTips;
+        if(customAmount.value === ""){
+            calcTips = calcOutputTipPerPerson(billAmount.value, value, peopleAmount.value);
+        }else{
+            calcTips = calcOutputTipPerPerson(billAmount.value, customAmount.value, peopleAmount.value)
+        }
+        outputTipPerPerson.innerHTML = calcTips.toFixed(2);
+        outputTotalPerPerson.innerHTML = ((((billAmount.value * 100) / peopleAmount.value) / 100) + calcTips).toFixed(2);
+    } 
+}
+
+function calcOutputTipPerPerson(billAmount, tipPercentage, peopleAmount){
+    return ((((billAmount * 100) * (tipPercentage / 100)) / 100) / peopleAmount);
+}
+
+function highlightReset(){
+    let outputTipPerPerson = document.querySelector(".js-tip-amount");
+    let outputTotalPerPerson = document.querySelector(".js-total-amount");
+    let resetButton = document.querySelector(".reset");
+    if (outputTipPerPerson.innerHTML !== "$0.00" && outputTotalPerPerson !== "$0.00"){
+        resetButton.style.opacity = "1";
+    }else{
+        resetButton.style.opacity = "0.5";
+    }
+}
+
+let tipAmountSpan = document.querySelector('.js-tip-amount');
+let totalAmountSpan = document.querySelector('.js-total-amount');
+tipAmountSpan.addEventListener('DOMSubtreeModified', highlightReset);
+totalAmountSpan.addEventListener('DOMSubtreeModified', highlightReset);
+
+function resetBill(){
+    let billAmount = document.querySelector(".js-bill-amount");
+    let peopleAmount = document.querySelector(".js-error-input");
+    let outputTipPerPerson = document.querySelector(".js-tip-amount");
+    let outputTotalPerPerson = document.querySelector(".js-total-amount");
+    billAmount.value = "";
+    removeCheckedState();
+    removeCustomAmount();
+    peopleAmount.value = "";
+    outputTipPerPerson.innerHTML = "$0.00";
+    outputTotalPerPerson.innerHTML = "$0.00";
+}
